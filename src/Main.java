9400c2deeb49354aaef77047632326ab2fb16cfd
ANGEL LING FANG QI <afqling@gmail.com>
2025-12-27 21:27:44 +0800

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+
+// ONLY THIS FILE WILL HAVE MAIN METHOD
+// USE FOR CALLING METHODS AFTER FINISHED ALL PARTS !!
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // ONLY THIS FILE WILL HAVE MAIN METHOD
-        // USE FOR CALLING METHODS AFTER FINISHED ALL PARTS !!
+    public static void main(String[] args) {
+        new LoginView(new Login("UserData.txt"));
     }
 }
+
